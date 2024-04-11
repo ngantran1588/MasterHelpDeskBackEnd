8f563f22e9e2d47891d7b3389c8daf27c59ee3f2
Julia1588 <congchuadochoi@gmail.com>
2024-04-11 12:04:17 -0600

    feat/api-server-proxy

--- src/database/organization.py
+++ src/database/organization.py
@@ -259,9 +259,9 @@ class Organization:
 
     def get_user_roles(self, username: str):
         query = """
-            SELECT r.role_name 
+            SELECT DISTINCT r.role_name 
             FROM tbl_customer c 
-            JOIN tbl_role r ON c.role_id = r.role_id 
+            JOIN tbl_role r ON r.role_id = ANY(c.role_id) 
             WHERE c.username = %s
         """
         values = (username,)
@@ -270,6 +270,9 @@ class Organization:
             result = self.db.execute_query(query, values)
             if len(result) == 0:
                 return None
-            return [role[0] for role in result]
+            
+            # Extract role names from the result
+            role_names = [role[0] for role in result]
+            return role_names
         except Exception as e:
             print("Error getting user roles:", e)
--- src/server_management/server_manager.py
+++ src/server_management/server_manager.py
@@ -47,7 +47,8 @@ class ServerManager:
             # Read stdout and stderr
             stdout_data = stdout.read().decode()
             stderr_data = stderr.read().decode()
-
+            if stderr_data:
+                print(stderr_data)
             # Return stdout and stderr data
             return stdout_data
         except Exception as e:
