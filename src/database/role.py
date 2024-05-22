a61f7f04b1e2694315a64a874f85cb9f543982b0
ngantran1588 <ngantranbao1588@gmail.com>
2024-05-22 13:14:58 +0700

    fix : api v1 role

--- src/database/role.py
+++ src/database/role.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+import re
 from . import connector
 from ..const import const
 from ..models.auth import Auth as AuthAPI
@@ -48,10 +48,38 @@ class Role:
         except Exception as e:
             print(f"Error getting role with ID {role_id}:", e)
 
+    def generate_id(self) -> str:
+        query = """SELECT MAX(role_id) FROM tbl_role"""
+      
+        result = self.db.execute_query(query)
+
+        if result == None:
+            return const.ROLE_ID_SUPER_USER
+
+        last_id = result[0][0]  # Fetch the first element (the ID)
+
+        # Extract the numeric part from the ID (assuming format "R" followed by digits)
+        if last_id:
+            match = re.search(r"R(\d{1,3})", last_id)  # Regex to extract up to 3 digits
+            if match:
+                numeric_part = int(match.group(1))
+            else:
+                raise ValueError("Invalid ID format encountered.")
+        else:
+            numeric_part = 0  # Start at 0 if no IDs exist
+
+        # Generate the next ID with leading zeros for 3 digits
+        next_id = f"R{numeric_part + 1:03d}"
+
+        return next_id
+
     def add_role(self, role_name, description):
 
-        role_id = self.auth.generate_id(role_name)
+        role_id = self.generate_id()
 
+        if role_id == None:
+            return False
+       
         query = """INSERT INTO tbl_role (role_id, role_name, description) VALUES (%s, %s, %s)"""
         values = (role_id, role_name, description)
 
